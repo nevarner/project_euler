ad622f6c68fa025453d504afdb60616cd9ecdabf
nevarner <nevarner@gmail.com>
2021-11-24 20:41:12 -0800

    added problem 2 from projecteuler.net

--- python_solutions/main.py
+++ python_solutions/main.py
@@ -2,9 +2,10 @@
 
 
 from problem_1 import problem_1
+from problem_2 import problem_2
 
 
-functions = [problem_1()]
+functions = [problem_1(), problem_2()]
 
 if __name__ == '__main__':
     for index, func in enumerate(functions):
---
+++ python_solutions/problem_2.py
@@ -0,0 +1,25 @@
+"""Problem 2 on ProjectEuler.net"""
+
+
+from functools import lru_cache
+
+
+@lru_cache(maxsize=3)
+def fib_recursion(n: int) -> int:
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_recursion(n-1) + fib_recursion(n-2)
+
+
+def problem_2(max_value: int = 4_000_000) -> int:
+    fib_sum = 0
+    i = 1
+    while fib_recursion(i) <= max_value:
+        if fib_recursion(i) % 2 == 0:
+            fib_sum += fib_recursion(i)
+        i += 1
+    return fib_sum
+
